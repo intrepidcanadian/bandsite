25919df126aa06cbd0a1f220be4cc5bdcf218ca6
intrepid <tony.kp.lau@gmail.com>
2023-06-12 01:37:00 -0400

    finished shows page

--- script.js
+++ script.js
@@ -4,7 +4,19 @@ const commentsArr = [
     {
         name: "Miles Acost",
         comment: "I can't stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-        date: 12 / 20 / 2020
+        date: new Date("12/20/2020")
+    },
+
+    {
+        name: "Emilie Beach",
+        comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+        date: new Date("01/09/2021")
+    },
+
+    {
+        name: "Connor Walton",
+        comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+        date: new Date("02/17/2021")
     }
 
 ];
@@ -13,34 +25,97 @@ const name__submission = document.getElementById("name__submission");
 const comment__submission = document.getElementById("comment__submission");
 const buttonSubmit = document.getElementById("comment_box");
 
-function displayComment(name, comment) {
+function displayComment(array,number) {
 
-    const commentElement = document.createElement("div");
+    const commentElement = document.getElementById("submitted_comment_container");
+    
+    const commentSection = document.createElement("div");
+    const commentSectionBottom = document.createElement("div");
+    const commentAvatar = document.createElement("div");
     const commentImg = document.createElement("img");
-    const commentDate = document.createElement("span");
-    const commentSection = document.getElementById("comment_section");
+    const placeholder =document.createElement("div");
+    const commentSubmitter = document.createElement("div")
+    const commentDate = document.createElement("div")
+    const commentComment = document.createElement("div")
 
-    const currentDate = new Date();
-    const formattedDate = currentDate.toDateString();
+    const commentDateValue = new Date(array[number].date)
+    const formattedDate = commentDateValue.toLocaleDateString("en-US", {
+        year: "numeric",
+        month: "long",
+        day: "numeric"
+      });
 
-    commentImg.src = "/BandSite-Package-Sprint-1/assets/images/Mohan-muruge.jpg"
-    commentElement.innerText = `${name} and ${comment}`;
-    commentElement.classList.add("commentbox");
-    commentDate.innerText = `${formattedDate}`
+    commentComment.innerText = array[number].comment;
+    commentDate.innerText = formattedDate;
+    commentSubmitter.innerText = array[number].name;
+    
 
-    commentSection.appendChild(commentImg);
-    commentSection.appendChild(commentElement);
-    commentSection.appendChild(commentDate)
+    commentSection.classList.add("submitted_container")
+    commentSectionBottom.classList.add("submitted_containerBottom")
+    commentAvatar.classList.add("submitted_avatar");
+    commentImg.classList.add("submitted_img");
+    placeholder.classList.add("placeholder");
+    commentComment.classList.add("submitted_comment");
+    commentSubmitter.classList.add("submitted_name");
+    commentDate.classList.add("submitted_date");
 
+    // commentSection.appendChild(commentImg);
+  
+    commentAvatar.appendChild(commentImg);
+    commentSection.appendChild(commentAvatar);
+    commentSection.appendChild(commentSubmitter);
+    commentSection.appendChild(commentDate);
+
+
+    commentElement.appendChild(commentSection);
+
+    commentSectionBottom.appendChild(placeholder);
+    commentSectionBottom.appendChild(commentComment);
+    commentElement.appendChild(commentSectionBottom);
 }
 
 const commentSubmission = buttonSubmit.addEventListener("submit", (event) => {
     event.preventDefault();
-    const name = name__submission.value;
-    const comment = comment__submission.value;
 
-    displayComment(name,comment)
-    // // for (i=0,i =1, i++)
-    // displayComment(comments[0]);
+  
 
-});
+    const name_input = name__submission.value;
+    const comment_input = comment__submission.value;
+    const currentDate = new Date();
+    const formattedDate = currentDate.toDateString();
+
+    const inputComment =
+    {
+        name: name_input,
+        comment: comment_input,
+        date: formattedDate
+    }
+
+    // add into new array the input boxes
+    commentsArr.push(inputComment);
+
+  
+    // clear current HTML
+
+    const commentElement = document.getElementById("submitted_comment_container");
+    commentElement.innerHTML =""
+
+
+    const sortedComments = commentsArr.sort((a, b) => {
+        return new Date(b.date) - new Date(a.date);
+
+    });
+
+    // i need to loop so that the array is appended in.
+
+    for (let i in commentsArr) {
+
+        console.log(sortedComments[i]);
+        displayComment(sortedComments, i)
+        
+    }
+
+    name__submission.value = "";
+    comment__submission.value = "";
+
+})
--- sound.js
+++ sound.js
@@ -1,5 +1,128 @@
-const BASE_URL = "https://newsapi.org/";
-const API_KEY = "c4ca425d2d1141be8dba919669c234ae";
+const concerts = [
+    {
+      date: "Mon Sept 06 2021",
+      venue: "Ronald Lane",
+      location: "San Francisco, CA"
+    },
+    {
+      date: "Tue Sept 21 2021",
+      venue: "Pier 3 East",
+      location: "San Francisco, CA "
+    },
+    {
+        date: "Fri Oct 15 2021",
+        venue: "View Lounge",
+        location: "San Francisco, CA "
+    },
+    {
+        date: "Sat Nov 06 2021 ",
+        venue: "Hyatt Agency",
+        location: "San Francisco, CA "
+    },
+  
+    {
+        date: "Fri Nov 26 2021",
+        venue: "Moscow Center",
+        location: "San Francisco, CA "
+    },
+    {
+        date: "Wed Dec 15 2021 ",
+        venue: "Press Club ",
+        location: "San Francisco, CA "
+    },
 
-axios
-.get(`${BASE_URL}/v2/top-headlines?country=ca&category=${category}&apiKey=${API_KEY}`)
+  ];
+
+// Function to create a concert row. the style will then be flexed column;
+function createConcertRow(arrayConcert) {
+
+// Create divs for all the rows to append to
+    const row = document.createElement("div");
+    row.classList.add("concert-row");
+  
+// Create elements within the divs
+    const dateCell = document.createElement("div");
+    dateCell.innerText = arrayConcert.date;
+    dateCell.classList.add("boldDate");
+    row.appendChild(dateCell);
+  
+    const venueCell = document.createElement("div");
+    venueCell.innerText = arrayConcert.venue;
+    venueCell.classList.add("cell");
+    row.appendChild(venueCell);
+  
+    const locationCell = document.createElement("div");
+    locationCell.innerText = arrayConcert.location;
+    locationCell.classList.add("cell");
+    row.appendChild(locationCell);
+  
+    const buyTicketsCell = document.createElement("div");
+    const buyTicketsButton = document.createElement("button");
+    buyTicketsButton.textContent = "BUY TICKETS";
+    buyTicketsButton.classList.add("buy-tickets-button");
+    buyTicketsCell.appendChild(buyTicketsButton);
+    buyTicketsCell.classList.add("cell");
+    row.appendChild(buyTicketsCell);
+  
+    // Add event listener to the row to handle row selection
+    
+    row.addEventListener("click", () => {
+      // Remove "selected" class from all rows
+      const rows = document.querySelectorAll(".concert-row");
+      rows.forEach(row => row.classList.remove("selected"));
+  
+      // Add "selected" class to the clicked row
+      row.classList.add("selected");
+    });
+  
+    return row;
+  }
+  
+  // Function to render the concerts table
+  function renderConcertsTable(concerts) {
+    const concertsContainer = document.getElementById("concertsContainer");
+  
+    // Create table header
+    const headerRow = document.createElement("div");
+    headerRow.classList.add("header-row");
+  
+    const dateHeader = document.createElement("div");
+    dateHeader.innerText = "DATE";
+    dateHeader.classList.add("header-cell");
+    headerRow.appendChild(dateHeader);
+  
+    const venueHeader = document.createElement("div");
+    venueHeader.innerText = "VENUE";
+    venueHeader.classList.add("header-cell");
+    headerRow.appendChild(venueHeader);
+  
+    const locationHeader = document.createElement("div");
+    locationHeader.innerText = "LOCATION";
+    locationHeader.classList.add("header-cell");
+    headerRow.appendChild(locationHeader);
+  
+    const buyTicketsHeader = document.createElement("div");
+    buyTicketsHeader.textContent = "";
+    buyTicketsHeader.classList.add("header-cell");
+    headerRow.appendChild(buyTicketsHeader);
+  
+    concertsContainer.appendChild(headerRow);
+  
+    // Create table rows for each concert by appending the latest concertRow
+    concerts.forEach(concert => {
+      const concertRow = createConcertRow(concert);
+      concertsContainer.appendChild(concertRow);
+    });
+  }
+  
+  // Render the concerts table by passing in the concerts array
+  renderConcertsTable(concerts);
+
+
+
+
+
+
+
+
+
